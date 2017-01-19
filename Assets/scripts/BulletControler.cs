70156d7422b321ac14f48e6e438f7f5cacf94744
sg13 <me>
2017-01-19 12:45:56 +0100

    import unity project

---
+++ Assets/scripts/BulletControler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControler : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * moveSpeed;
+    }
+
+    void Update() {
+        if (rb.position.z > 18) {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/scripts/PlayerControler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BoundaryArea
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerControler : MonoBehaviour {
+
+    public float moveSpeed;
+    public float moveTilt;
+    public BoundaryArea boundaryArea;
+
+    public GameObject bullet;
+    public float fireRate;
+
+    private Rigidbody playerRigidbody;
+    private float nextFire;
+
+    // Use this for initialization
+    void Start () {
+        playerRigidbody = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1") && Time.time > nextFire) {
+            //bullet start pos
+            Vector3 bulletStart = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 1);
+            //next fire
+            nextFire = Time.time + fireRate;
+
+            //clone prefab bullet
+            Instantiate(bullet, bulletStart, this.transform.rotation);
+            
+        }
+    }
+
+    // call each fixed physics step
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 moves = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        playerRigidbody.velocity = moves * moveSpeed;
+        
+        playerRigidbody.position = new Vector3(
+                Mathf.Clamp(playerRigidbody.position.x, boundaryArea.xMin, boundaryArea.xMax),
+                0.0f,
+                Mathf.Clamp(playerRigidbody.position.z, boundaryArea.zMin, boundaryArea.zMax)
+            );
+
+        playerRigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, playerRigidbody.velocity.x * moveTilt);
+    }
+}
